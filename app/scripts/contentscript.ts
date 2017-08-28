64caddc03fb65ae0bd4cd653ec5740725b7c78c2
Shota Miyazaki <jmsf0203@gmail.com>
2017-08-29 01:03:30 +0900

    Completed console log version

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,1 +1,47 @@
-console.log('\'Allo \'Allo! Content script')
+initializeWhenReady(document);
+
+class Atumori {
+  private videoDom: HTMLVideoElement;
+
+  constructor(videoDom: HTMLVideoElement) {
+    this.videoDom = videoDom;
+    this.videoDom.addEventListener("play", (event) => this.showAtsumori(event))
+  }
+
+  private showAtsumori(event: Event) {
+    setTimeout(() => {
+      if (!this.videoDom.paused) {
+        console.log(event)
+        console.log("10秒後");
+      }
+    }, 5*1000);
+  }
+}
+
+function initializeWhenReady(document: Document) {
+  const readyStateCheckInterval = setInterval(function() {
+    if (document && document.readyState === 'complete') {
+      clearInterval(readyStateCheckInterval);
+      initializeNow(document);
+    }
+  }, 10);
+}
+
+function initializeNow(document: Document) {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      mutation.addedNodes.forEach(node => {
+        if (node.nodeName === 'VIDEO') {
+          new Atumori(node as HTMLVideoElement);
+        }
+      });
+    });
+  });
+
+  observer.observe(document, { childList: true, subtree: true });
+
+  const videoTags = document.getElementsByTagName('video');
+  Array.from(videoTags).forEach(video => {
+    new Atumori(video);
+  });
+}
